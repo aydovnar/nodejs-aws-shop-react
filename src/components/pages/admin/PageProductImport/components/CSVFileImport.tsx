7c918b0bd3257e2e511cc31f9888be3954e58c2f
artsem_dounar <artsem.dounar@injazat.com>
2025-03-22 11:48:36 +0300

    Task-7: added header support and additional toast on errors(401 & 403)

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,22 +31,61 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
     console.log("uploadFile to", url);
 
+    const token = localStorage.getItem("authorization_token");
+    let headers = {};
+    if (token) headers = { Authorization: `Basic ${token}` };
+
     // Get the presigned URL
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        name: encodeURIComponent(file.name),
-      },
-    });
-    console.log("File to upload: ", file.name);
-    console.log("Uploading to: ", response.data);
-    const result = await fetch(response.data, {
-      method: "PUT",
-      body: file,
-    });
-    console.log("Result: ", result);
-    setFile(undefined);
+    try {
+      const response = await axios({
+        method: "GET",
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
+        headers: headers,
+      });
+
+      // Handle the successful response
+      console.log(response.data);
+      console.log("File to upload: ", file.name);
+      console.log("Uploading to: ", response.data);
+      const result = await fetch(response.data, {
+        method: "PUT",
+        body: file,
+      });
+      console.log("Result: ", result);
+      setFile(undefined);
+    } catch (error: any) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        if (error.response.status === 401) {
+          // Handle 401 Unauthorized error
+          window.dispatchEvent(
+            new CustomEvent("global-toast", {
+              detail: { message: "401 Unauthorized", severity: "error" },
+            })
+          );
+        } else if (error.response.status === 403) {
+          // Handle 403 Forbidden error
+          window.dispatchEvent(
+            new CustomEvent("global-toast", {
+              detail: { message: "403 Forbidden", severity: "error" },
+            })
+          );
+        } else {
+          // Handle other error responses
+          console.error("Error:", error.response.data);
+        }
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("No response received:", error.request);
+      } else {
+        // Something else happened in making the request
+        console.error("Error:", error.message);
+      }
+    }
   };
   return (
     <Box>
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,24 @@ import App from "~/components/App/App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { MutationCache, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import ToastProvider from "~/components/ToastProvider/ToastProvider";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: { refetchOnWindowFocus: false, retry: false, staleTime: Infinity },
   },
+  mutationCache: new MutationCache({
+    onError: (error: any) => {
+      window.dispatchEvent(
+        new CustomEvent("global-toast", {
+          detail: { message: error.message, severity: "error" },
+        })
+      );
+    },
+  }),
 });
 
 (async () => {
@@ -29,7 +39,9 @@ const queryClient = new QueryClient({
         <QueryClientProvider client={queryClient}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <App />
+            <ToastProvider>
+              <App />
+            </ToastProvider>
           </ThemeProvider>
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>

ab9310f8c42159c9eb1707082b3d3b768b453afa
artsem_dounar <artsem.dounar@injazat.com>
2025-04-14 10:11:04 +0300

    Task-9: sm fix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://w1ikol8pue.execute-api.eu-central-1.amazonaws.com/prod",
   order:
-    "http://aydovnar-cart-api-develop.eu-central-1.elasticbeanstalk.com//prod/profile/cart",
+    "http://aydovnar-cart-api-develop.eu-central-1.elasticbeanstalk.com/prod/profile/cart",
   import: "https://w2kepyqpj4.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://aydovnar-cart-api-develop.eu-central-1.elasticbeanstalk.com//prod",
+  cart: "http://aydovnar-cart-api-develop.eu-central-1.elasticbeanstalk.com/prod",
 };
 
 export default API_PATHS;
